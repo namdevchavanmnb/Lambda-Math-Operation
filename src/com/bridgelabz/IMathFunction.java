44b44ef20f7134f8bdf5168e5e18ee89f3ad3832
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-20 07:42:52 +0530

    Added:ability to show the result

--- src/com/bridgelabz/IMathFunction.java
+++ src/com/bridgelabz/IMathFunction.java
@@ -2,6 +2,9 @@ package com.bridgelabz;
 @FunctionalInterface
  interface IMathFunction {
     int calculate(int a,int b);
+    static void printResult(int a,int b,String function,IMathFunction ref){
+        System.out.println("Result of "+function+" is "+ref.calculate(a,b));
+    }
 }
 
 
@@ -10,6 +13,13 @@ class Demo{
         IMathFunction sum=(x,y) ->(x+y);
         IMathFunction multiply=(x,y) ->(x*y);
         IMathFunction divide=(x,y) ->(x/y);
+
+        System.out.println(sum.calculate(4,5));
+        System.out.println(multiply.calculate(6,6));
+        System.out.println(divide.calculate(7,7));
+        IMathFunction.printResult(8,6,"Sum >",sum);
+        IMathFunction.printResult(8,6,"Multiply >",multiply);
+        IMathFunction.printResult(8,8,"Divide >",divide);
 
     }
 }
